0dbfbd9d0279565542335bc384f162fb6eab3955
D4Techs <D4Techs@gmail.com>
2017-02-26 22:06:53 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+import android.os.*;
+import android.widget.*;
+import android.
+
+class MainActivity extends Activity
+{
+
+@Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.main);
+   }
+
+}
